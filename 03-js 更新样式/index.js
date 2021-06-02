fc552f1129dbe2d51608e01ba77a1f2b6226576b
shenshi <shenshi@shouqianba.com>
2021-06-02 18:28:35 +0800

    feat: 03 使用js更新样式

---
+++ 03-js 更新样式/index.js
@@ -0,0 +1,14 @@
+// 获取.control中所有input 
+const inputs = document.querySelectorAll('.control input')
+
+function handleUpdate () {
+  // 获取DOM元素 自定义属性data-sizing
+  const suffix = this.dataset.sizing || ''
+  // 设置样式属性 setProperty
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+
+  document.getElementById(`label_${this.name}`).innerText = this.value + suffix 
+}
+
+inputs.forEach(input => input.addEventListener('change',handleUpdate))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
